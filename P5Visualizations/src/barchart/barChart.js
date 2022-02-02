227d1b52c6de6184136d90269ac2448baf54c8e5
tlin41390 <56329045+tlin41390@users.noreply.github.com>
2022-02-01 23:59:21 -0800

    Add files via upload

---
+++ P5Visualizations/src/barchart/barChart.js
@@ -0,0 +1,45 @@
+let table;
+let totalCases;
+function preload(){
+  table = loadTable("../../CSV/covid_variants(total).csv","csv","header");
+}
+function setup() {
+  createCanvas(2200 , 2600);
+  background(220);
+  totalCases = [];
+  push();
+  let variants = "Variants";
+  translate(variants)
+  textStyle(BOLD);
+  textSize(20);
+  text(variants,25,2600/2);
+  pop();
+
+  for(let r = 0; r < table.getRowCount(); r++) {
+    const name = table.getString(r, "Variant");
+    const cases = table.getNum(r,"Cases");
+    
+    totalCases[r] = cases;
+    text(name, 100 , 50+r*100);
+    rect(200,100*r,totalCases[r]/2000, 100);
+    
+  }
+
+  const maxValue = max(totalCases);
+  for(let j = 0;j<maxValue;j = j + 200000){
+    text(j, 200 + j/2000,2500);
+  }
+  push();
+  textStyle(BOLD);
+  textSize(20);
+  text("Cases",2200/2,2550);
+  pop();
+}
+
+
+// function draw() {
+  
+//   for(let r = 0; r < table.getRowCount(); r++){
+
+//   }
+// }
