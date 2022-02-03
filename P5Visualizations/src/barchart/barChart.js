32b1372ba13514e738786ec504515941fced47e3
tlin41390 <tlin41@dons.usfca.edu>
2022-02-02 20:07:08 -0800

    updatedBarChart

--- P5Visualizations/src/barchart/barChart.js
+++ P5Visualizations/src/barchart/barChart.js
@@ -4,36 +4,64 @@ function preload(){
   table = loadTable("../../CSV/covid_variants(total).csv","csv","header");
 }
 function setup() {
-  createCanvas(2200 , 2600);
+  createCanvas(2200 , 2800);
   background(220);
   totalCases = [];
+
   push();
-  let variants = "Variants";
-  translate(variants)
   textStyle(BOLD);
   textSize(20);
-  text(variants,25,2600/2);
+  text("Cases For Each Variant",2200/2,25);
   pop();
 
-  for(let r = 0; r < table.getRowCount(); r++) {
-    const name = table.getString(r, "Variant");
-    const cases = table.getNum(r,"Cases");
-    
-    totalCases[r] = cases;
-    text(name, 100 , 50+r*100);
-    rect(200,100*r,totalCases[r]/2000, 100);
-    
-  }
+  push();
+  let variants = "Variants";
+  textStyle(BOLD);
+  textSize(20);
+  text(variants,25,50);
+  pop();
+
+  gatherData();
 
   const maxValue = max(totalCases);
-  for(let j = 0;j<maxValue;j = j + 200000){
-    text(j, 200 + j/2000,2500);
-  }
+  buildGraph(maxValue);
+  
   push();
   textStyle(BOLD);
   textSize(20);
   text("Cases",2200/2,2550);
   pop();
+}
+
+function gatherData(){
+  for(let r = 0; r < table.getRowCount(); r++){
+    totalCases[r] = table.getNum(r,"Cases");
+  }
+}
+
+function buildGraph(maxValue){
+  for(let j = 0;j<maxValue;j = j + 200000){
+    line(200 + j/2000,50,200+ j/2000,2450);
+    textStyle(BOLD);
+    text(j, 200 + j/2000,2500);
+  }
+
+  for(let r = 0; r < table.getRowCount(); r++) {
+    const name = table.getString(r, "Variant");
+    textStyle(BOLD);
+    text(name, 100, 100+r*100);
+    push();
+    let c = color(135,206,235);
+    fill(c);
+    rect(200,50+(100*r),totalCases[r]/2000, 100);
+    pop();
+  }
+}
+
+function labelGraph(label){
+  textStyle(BOLD);
+  textSize(20);
+  text("Cases",2200/2,2550);
 }
 
 
