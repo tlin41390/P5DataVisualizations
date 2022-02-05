49bf7e36f6bbb3a2510acd87fcd6d84eeaddf4aa
tlin41390 <tlin41@dons.usfca.edu>
2022-02-05 13:10:12 -0800

    created heatmap

--- P5Visualizations/src/heatmap/heatmap.js
+++ P5Visualizations/src/heatmap/heatmap.js
@@ -1,26 +1,36 @@
 let table;
 function preload(){
-  table = loadTable("../../CSV/covid-variants(byDate).csv","csv","header");
+  table = loadTable("https://gist.githubusercontent.com/tlin41390/2aca7bfbb746070cc2ced30aa9798a88/raw/61a26fefa365a01cad7b83aadff45772c37a32f5/covid-variants(date).csv","csv","header");
 }
 function setup() {
-  createCanvas(800 , 800);
+  createCanvas(2400 , 900);
   background(220);
-  const casesByDates = new Map();
-  for(let i =0;i<table.getRowCount(); i ++){ 
-      if(!casesByDates.has(table.getString(i,"Quarter of Date"))){
-          casesByDates.set(table.getString(i, "Quarter of Date"), new Map());
-      }
+  const variants = new Set();
+  const quarters = new Set();
+
+  for(let i = 0;i<table.getRowCount(); i++){
+    const name=table.getString(i,"Variant");
+    const quarter = table.getString(i,"Quarter of Date");
+    variants.add(name);
+    quarters.add(quarter);
   }
 
-  for(let j =0; j<table.getRowCount();j++){
-      if(!casesByDates.get(table.getString(j, "Quarter of Date")).has(table.getString(j, "Variant"))){
-        casesByDates.get(table.getString(j, "Quarter of Date")).set(table.getString(j,"Variant"),0);
-      }
+  for(let a = 0; a<variants.size; a++){ 
+    text(Array.from(variants)[a],100+ a *100,850);
   }
 
-//   for(let k = 0; k<table.getRowCount(); k++){
-//       casesByDates.get(table.getString(k, "Month of Date"))
-//   }
-  
-  console.log(casesByDates);
+  for(let b = 0;b<quarters.size; b++){ 
+    text(Array.from(quarters)[b],0,100+ b*100);
+  }
+
+  for(let row = 0; row<quarters.size; row++){
+    for(let col = 0; col < variants.size; col++){
+      rect(50,50 ,50,50);
+    }
+  }
+
+  console.log(variants);
+  console.log(quarters);
+  console.log(Array.from(variants)[0])
 }
+
